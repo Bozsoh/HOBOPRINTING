915ec355b8603c078eaa26737b9febe03923bf50
Bozsoh <bozoh@users.noreply.github.com>
2021-06-19 21:28:05 +1200

    Pin config ReadMe

--- pins.h
+++ pins.h
@@ -1198,7 +1198,7 @@
 
 #define LED_PIN            -1
 
-#define FAN_PIN            12
+#define FAN_PIN            4
  #if FAN_PIN == 12 || FAN_PIN ==13
   #define FAN_SOFT_PWM
 #endif
@@ -1243,11 +1243,11 @@
 
 #ifdef SANGUINOLOLU_V_1_2
 
- #define HEATER_BED_PIN     12 // (bed)
- #define X_ENABLE_PIN       14
- #define Y_ENABLE_PIN       14
- #define Z_ENABLE_PIN       26
- #define E0_ENABLE_PIN      14
+ #define HEATER_BED_PIN     14 // (bed)
+ #define X_ENABLE_PIN       -1
+ #define Y_ENABLE_PIN       -1
+ #define Z_ENABLE_PIN       -1
+ #define E0_ENABLE_PIN      -1
 
  #ifdef LCD_I2C_PANELOLU2
    #define FAN_PIN          4 // Uses Transistor1 (PWM) on Panelolu2's Sanguino Adapter Board to drive the fan
